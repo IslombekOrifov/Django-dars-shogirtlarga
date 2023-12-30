19df5ef1fd610eb1601091ee3debe6f4d47d7c8c
IslombekOrifov <islombekorifov0199@gmail.com>
2023-12-31 00:06:37 +0500

    4 -dars

--- blog-site/blog/models.py
+++ blog-site/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.urls import reverse
 
 
@@ -30,7 +30,6 @@ class Post(models.Model):
     objects = models.Manager() # менеджер, применяемый по умолчанию
     published = PublishedManager() # конкретно-прикладной менеджер
 
-    
     class Meta:
         ordering = ['-publish']
         indexes = [
@@ -45,3 +44,24 @@ class Post(models.Model):
                                                  self.publish.month, 
                                                  self.publish.day, 
                                                  self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blog-site/blog/views.py
+++ blog-site/blog/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
+from django.conf import settings
+from django.views.decorators.http import require_POST
 
-from blog.models import Post
+from blog.models import Post, Comment as CommentNew
+from .forms import EmailPostForm, CommentForm
 
 
 def post_list(request):
@@ -26,5 +30,59 @@ def post_detail(request, year, month, day, post_slug):
                              publish__month=month, 
                              publish__day=day, 
                              status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    # Форма для комментирования пользователями
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post, 
+                                                     'comments':comments,
+                                                     'form': form})
 
+
+
+def post_share(request, post_id):
+# Извлечь пост по его идентификатору id
+    post = get_object_or_404(Post, 
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            
+            subject = f"{cd['name']} recommends you read " \
+                f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                f"{cd['name']}\'s comments: {cd['comments']}"
+                
+            send_mail(subject, message, 'your_account@gmail.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 
+                                                    'form': form, 
+                                                    'sent': sent})
+    
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+    # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 
+                                                      'form': form,
+                                                      'comment': comment})
